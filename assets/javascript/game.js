10b13ae0eef0dca909a5b2661f4413e85c142319
chrisn27 <chris_nguyen27@yahoo.com>
2017-04-11 19:34:48 -0700

    kinda working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,13 @@ var options = ["cat", "dog", "hamster", "hedgehog", "fox", "ferret", "fish", "co
 "labrador", "voldemort", "slytherin", "hufflepuff", "gryffindor", "ravenclaw", "moldy", "wand",
 "beer", "tacos", "burrito", "moldy", "lamp", "packers", "ducks", "angels", "spurs", "avocado",
 "mazda", "bottle", "logic", "diablo", "wizard", "barbarian", "amazon", "druid", "sushi", "stark"];
-var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var userGuesses = [];
-var numGuesses = 0;
-var wordLength = "";
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var wordBlanks = [];
-var blanks= 0;
-var test = [];
-var setWord = [];
+var gameWord;
+var dummy = true;
 
     var reset = function() {
 
@@ -27,48 +23,80 @@ var setWord = [];
 
     // function called on load to
 
+    function reset () {
+        guessesLeft = 10;
+        userGuesses = [];
+        wordBlanks = [];
+        gameWord;
+        startGame();
+    }
+
     function startGame() {
-
-    guessesLeft = 10;
-    userGuesses = [];
-    
-    var gameWord = options[Math.floor(Math.random() * options.length)];
-    setWord.push(gameWord)
-
-    console.log(gameWord);
-
-    //splits word into array
-    var wordLength = gameWord.split("");
-
-    // I've got a blank space for you
-    blanks = wordLength.length;
-    for (var i = 0; i < blanks; i++) {
-    wordBlanks.push("_")
-
-    }
-
-    console.log(wordBlanks.join(" "));
-
-    document.querySelector("#targetword").innerHTML = wordBlanks.join("");
-    document.querySelector("#remaining").innerHTML = guessesLeft;
-
-    }
-
-	// Determines which key was pressed by user
-    document.onkeydown = function(event) {
         
-    // translates user input to lowercase
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        gameWord = options[Math.floor(Math.random() * options.length)];
 
-    // adds last user button down to UserGuesses and pushes guesses letters to page
-    userGuesses.push(userGuess);
-    var guessed = userGuesses.join(', ') + "</p>";
-    document.querySelector("#guessedLetters").innerHTML = guessed;
-    document.querySelector("#remaining").innerHTML = guessesLeft;
+        // I've got a blank space for you
+        for (var i = 0; i < gameWord.length; i++) {
+        wordBlanks.push("_")
+
+        }
+
+        // Determines which key was pressed by user
+};
+
+        document.onkeydown = function(event) {
+        
+        // translates user input to lowercase
+        userGuess = event.key.toLowerCase();
+        var guessIsGood = userGuess.match("^[a-zA-Z]*$");
+
+            if (guessIsGood) {
+
+                var arr = [];
+
+            if (gameWord.indexOf(userGuess) > -1){
+     
+                for (var i=0; i < gameWord.length; i++) {
+
+                    if (userGuess==gameWord.charAt(i)) {
+
+                        wordBlanks[i]=userGuess;
+
+                    } 
+                }
+
+
+                if (wordBlanks.join("") == gameWord) {
+                    alert("You win!, the word was " + gameWord);
+                    wins++;
+                    reset();
+                }
+
+                }  else {
+
+                guessesLeft--;
+
+                if (guessesLeft < 0) {
+                    alert("You lose!");
+                    losses++;
+                    reset();   
+                }
+
+                }
+
+                console.log(wordBlanks.join("")); 
+
+                userGuesses.push(userGuess);
+                var guessed = userGuesses.join(', ') + "</p>";
+                document.querySelector("#guessedLetters").innerHTML = guessed;
+                document.querySelector("#wins").innerHTML = "Wins: " + wins;
+                document.querySelector("#losses").innerHTML = "Losses: " + losses;
+                document.querySelector("#remaining").innerHTML = guessesLeft;
+                document.querySelector("#targetword").innerHTML = wordBlanks.join("");
+                document.querySelector("#remaining").innerHTML = guessesLeft;
+                console.log(gameWord);
     
-    // testing setWord global access
-    console.log(setWord);
-    
-    };
+            }
+        };
 
           
